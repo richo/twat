77414bca5237e05acdccbac9a44be6f251bb1bd7
richo <richo@psych0tik.net>
2011-09-21 12:44:45 +1000

    BUGFIX- didn't actually show user

--- lib/twat/actions.rb
+++ lib/twat/actions.rb
@@ -46,7 +46,7 @@ module Twat
     def show(opts)
       twitter_auth(opts)
       Twitter.home_timeline.each_with_index do |tweet, idx|
-        puts "#{tweet.from_user}: #{tweet.text}"
+        puts "#{tweet.user.screen_name}: #{tweet.text}"
 
         break if idx == opts[:count]
       end
