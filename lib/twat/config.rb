6682e9a6f1b90cc2cb42e552c50ce659e3df3f2d
richo <richo@psych0tik.net>
2011-10-17 19:53:33 +1100

    Add identi.ca credentials

--- lib/twat/config.rb
+++ lib/twat/config.rb
@@ -71,8 +71,8 @@ module Twat
         },
         :"identi.ca" => # FIXME
         {
-          consumer_key: "jtI2q3Z4NIJAehBG4ntPIQ",
-          consumer_secret: "H6vQOGGn9qVJJa9lWyTd2s8ZZRXG4kh3SMfvZ2uxOXE"
+          consumer_key: "0af040d41599f5d07b738fc90a487af7",
+          consumer_secret: "b6bb7284eaf975ab65a2dd3eb53fbf3d"
         }
       }
     end
