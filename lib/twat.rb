41faf88a9bccda6a4bb378f54b6d84e56dff46f7
richo <richo@psych0tik.net>
2011-10-17 18:17:49 +1100

    Refactor the exception handling

--- lib/twat.rb
+++ lib/twat.rb
@@ -49,44 +49,16 @@ module Twat
 
   VERSION = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_PATCH}"
   class Twat
+
+    include HandledExceptions
+
     def cli_run
-      begin
-        opts = ArgParse.new
+      # FIXME Handing in the opts like this feels dirty
+      with_handled_exceptions(ArgParse.new) do |opts|
         actor = Actions.new
         actor.config = config
         actor.opts = opts
         actor.send(opts.options[:action])
-      rescue Usage
-        opts.usage
-      rescue NoSuchAccount
-        puts "No such account"
-        opts.usage
-      rescue NoDefaultAccount
-        puts "No default account configured."
-      rescue NoSuchCommand
-        puts "No such command"
-        opts.usage
-      rescue NoConfigFile
-        puts "No config file, create one with twat -a [user|nick]"
-        opts.usage
-      rescue InvalidSetOpt
-        puts "There is no such configurable option"
-        opts.usage
-      rescue RequiresOptVal
-        puts "--set must take an option=value pair as arguments"
-      rescue InvalidCredentials
-        puts "Invalid credentials, try reauthenticating with"
-        puts "twat -a #{opts[:account]}"
-      rescue ConfigVersionIncorrect
-        puts "Your config file is out of date. Run with --update-config to rememdy"
-      rescue InvalidBool
-        puts "Invalid value, valid values are #{Options::BOOL_VALID.join("|")}"
-      rescue InvalidInt
-        puts "Invalid value, must be an integer"
-      rescue Errno::ECONNRESET
-        puts "Connection was reset by third party."
-      rescue TweetTooLong
-        puts "Twitter enforces a maximum status length of 140 characters"
       end
     end
   end
--- lib/twat/exceptions.rb
+++ lib/twat/exceptions.rb
@@ -11,4 +11,44 @@ module Twat
   class InvalidBool < Exception; end
   class InvalidInt < Exception; end
   class TweetTooLong < Exception; end
+
+  module HandledExceptions
+    def with_handled_exceptions(opts)
+      begin
+        # FIXME
+        yield opts
+      rescue Usage
+        opts.usage
+      rescue NoSuchAccount
+        puts "No such account"
+        opts.usage
+      rescue NoDefaultAccount
+        puts "No default account configured."
+      rescue NoSuchCommand
+        puts "No such command"
+        opts.usage
+      rescue NoConfigFile
+        puts "No config file, create one with twat -a [user|nick]"
+        opts.usage
+      rescue InvalidSetOpt
+        puts "There is no such configurable option"
+        opts.usage
+      rescue RequiresOptVal
+        puts "--set must take an option=value pair as arguments"
+      rescue InvalidCredentials
+        puts "Invalid credentials, try reauthenticating with"
+        puts "twat -a #{opts[:account]}"
+      rescue ConfigVersionIncorrect
+        puts "Your config file is out of date. Run with --update-config to rememdy"
+      rescue InvalidBool
+        puts "Invalid value, valid values are #{Options::BOOL_VALID.join("|")}"
+      rescue InvalidInt
+        puts "Invalid value, must be an integer"
+      rescue Errno::ECONNRESET
+        puts "Connection was reset by third party."
+      rescue TweetTooLong
+        puts "Twitter enforces a maximum status length of 140 characters"
+      end
+    end
+  end
 end
