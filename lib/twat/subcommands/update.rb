5e08eef7fb87b3dba0837b4e42c11508f8724fce
richo <richo@psych0tik.net>
2011-10-21 08:24:14 +1100

    Port update code to new design

---
+++ lib/twat/subcommands/update.rb
@@ -0,0 +1,17 @@
+module Twat::Subcommands
+  class Add < Base
+
+    def initialize
+    end
+
+    def run
+      twitter_auth
+
+      raise TweetTooLong if opts.msg.length > 140
+
+      Twitter.update(opts.msg)
+      #puts opts.msg
+    end
+  end
+  COMMANDS['update'] = Add
+end
