620b7f98e434633917a2429400bbddc4039ef962
richo <richo@psych0tik.net>
2012-02-12 10:52:41 +1100

    Catch twitter error

--- lib/twat/actions/follow.rb
+++ lib/twat/actions/follow.rb
@@ -79,16 +79,15 @@ module Twat
           failcount = 0
         rescue Interrupt
           break
-        # rescue Twitter::ServiceUnavailable
-        #   if failcount > 2
-        #     puts "3 consecutive failures, giving up"
-        #   else
-        #     sleeptime = 60 * (failcount + 1)
-        #     print "(__-){".red
-        #     puts ": the fail whale has been rolled out, sleeping for #{sleeptime} seconds"
-        #     sleep sleeptime
-        #     failcount += 1
-        #   end
+        rescue Twitter::Error::ServiceUnavailable
+          if failcount > 2
+            puts "3 consecutive failures, giving up"
+          else
+            sleeptime = 60 * (failcount + 1)
+            @reader.puts_above "#{"(__-){".red}: the fail whale has been rolled out, sleeping for #{sleeptime} seconds"
+            sleep sleeptime
+            failcount += 1
+          end
         rescue Errno::ECONNRESET
         rescue Errno::ETIMEDOUT
           if failcount > 2
