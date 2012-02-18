a05b27ebcd2e4886532ebd23c394fbb15f745e7f
richo <richo@psych0tik.net>
2012-02-18 19:20:19 +1100

    Refactor the way twat handles authentication
    Inside the endpoint model makes far more sense

--- lib/twat/endpoints/base.rb
+++ lib/twat/endpoints/base.rb
@@ -3,5 +3,26 @@ module Twat::Endpoints
 
     @@endpoint_set=nil
 
+    def authorize_account(name)
+      oauth_conf = oauth_options.merge ({ :site => url })
+
+      oauth = OAuth::Consumer.new( *[*consumer_info.values, oauth_conf] )
+      token_request = oauth.get_request_token()
+      puts "Please authenticate the application at #{token_request.authorize_url}, then enter pin"
+      pin = STDIN.gets.chomp
+      begin
+        access_token = token_request.get_access_token(oauth_verifier: pin)
+        account_settings = {
+          oauth_token: access_token.token,
+          oauth_token_secret: access_token.secret,
+          endpoint: opts[:endpoint]
+        }
+        config.accounts[opts[:account]] = account_settings
+        config.save!
+      rescue OAuth::Unauthorized
+        puts "Couldn't authenticate you, did you enter the pin correctly?"
+      end
+    end
+
   end
 end
