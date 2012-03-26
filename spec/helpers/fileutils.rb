8e8c6961a7b92c82154d8f7b3ae205da578dedd1
richo <richo@psych0tik.net>
2012-03-27 10:08:59 +1100

    Try mktemp with no prefix

--- spec/helpers/fileutils.rb
+++ spec/helpers/fileutils.rb
@@ -3,7 +3,7 @@ module FileUtils extend self
   # Shamelessly poached from homebrew
   def mktemp(opts={})
     tmp_prefix = ENV['TWAT_TEMP'] || Dir.pwd
-    tmp=Pathname.new `/usr/bin/mktemp -d #{tmp_prefix}/twat_spec-XXXX`.strip
+    tmp=Pathname.new `mktemp -d #{tmp_prefix}/twat_spec-XXXX`.strip
     raise "Couldn't create build sandbox" if not tmp.directory? or $? != 0
     begin
       yield tmp
