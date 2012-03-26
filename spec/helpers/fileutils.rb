b5acaece44dcad93f758b73752da7f6d0a943f7e
richo <richo@psych0tik.net>
2012-03-27 09:12:44 +1100

    Make temp directories off current dir for travis

--- spec/helpers/fileutils.rb
+++ spec/helpers/fileutils.rb
@@ -2,7 +2,7 @@ module FileUtils extend self
 
   # Shamelessly poached from homebrew
   def mktemp(opts={})
-    tmp_prefix = ENV['HOMEBREW_TEMP'] || '/tmp'
+    tmp_prefix = ENV['TWAT_TEMP'] || Dir.pwd
     tmp=Pathname.new `/usr/bin/mktemp -d #{tmp_prefix}/twat_spec-XXXX`.strip
     raise "Couldn't create build sandbox" if not tmp.directory? or $? != 0
     begin
