2115eb70c961aec14981ab289a51a1a32b71e14f
richo <richo@psych0tik.net>
2012-03-26 23:34:32 +1100

    restructure spec expectation

--- spec/argparse_spec.rb
+++ spec/argparse_spec.rb
@@ -1,32 +1,32 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-MULTIUSER_CONFIG = { accounts: {
-        :rich0H =>
-          { oauth_token: "rich0h's token",
-            oauth_token_secret: "rich0H's secret",
-            endpoint: :twitter
-          },
-        :secondAccount =>
-          { oauth_token: "secondAccount's token",
-            oauth_token_secret: "secondAccount's secret",
-            endpoint: :twitter
-          }
-      },
-      :default => :rich0H
-}
-
 describe Twat do
+
+  multiuser_config = { accounts: {
+    :rich0H =>
+    { oauth_token: "rich0h's token",
+      oauth_token_secret: "rich0H's secret",
+      endpoint: :twitter
+    },
+      :secondAccount =>
+    { oauth_token: "secondAccount's token",
+      oauth_token_secret: "secondAccount's secret",
+      endpoint: :twitter
+    }
+  },
+    :default => :rich0H
+  }
 
   ["-n", "--account"].each do |flag|
     it "Should respect the #{flag} flag" do
-      with_config(MULTIUSER_CONFIG) do
+      with_config(multiuser_config) do
 
         set_argv [flag, "secondAccount"]
 
         $args = ::Twat::Args.new
         conf = ::Twat::Config.new
 
-        conf.account.should == MULTIUSER_CONFIG[:accounts][:secondAccount]
+        conf.account.should == multiuser_config[:accounts][:secondAccount]
       end
     end
   end
