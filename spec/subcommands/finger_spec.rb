eb9e26750c7c33065e7a6e06917ffd06ce3ce330
richo <richo@psych0tik.net>
2012-03-26 23:34:47 +1100

    Finger spec inherits config

--- spec/subcommands/finger_spec.rb
+++ spec/subcommands/finger_spec.rb
@@ -2,32 +2,53 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Twat do
 
-  it "Should bail if finger is called without an argument" do #{{{
-    set_argv ["finger"]
-    STDOUT.expects(:puts).with(Twat::Subcommands::Finger.usage)
+  multiuser_config = { accounts: {
+    :rich0H =>
+    { oauth_token: "rich0h's token",
+      oauth_token_secret: "rich0H's secret",
+      endpoint: :twitter
+    },
+      :secondAccount =>
+    { oauth_token: "secondAccount's token",
+      oauth_token_secret: "secondAccount's secret",
+      endpoint: :twitter
+    }
+  },
+    :default => :rich0H
+  }
 
-    lambda { Twat::Twat.new.cli_run }.should raise_error SystemExit
+  it "Should bail if finger is called without an argument" do #{{{
+    with_config(multiuser_config) do
+      set_argv ["finger"]
+      STDOUT.expects(:puts).with(Twat::Subcommands::Finger.usage)
+
+      lambda { Twat::Twat.new.cli_run }.should raise_error SystemExit
+    end
   end #}}}
 
   it "Should retrieve a tweet for user if called with only a user" do #{{{
-    tweet = mock()
-    tweet.expects(:each).yields(tweet)
-    Twitter.expects(:user_timeline).with("hanke", :count => 1).returns(tweet)
-    Twat::Subcommands::Finger.any_instance.expects(:format).with(tweet)
+    with_config(multiuser_config) do
+      tweet = mock()
+      tweet.expects(:each).yields(tweet)
+      Twitter.expects(:user_timeline).with("hanke", :count => 1).returns(tweet)
+      Twat::Subcommands::Finger.any_instance.expects(:format).with(tweet)
 
-    set_argv ["finger", "hanke"]
+      set_argv ["finger", "hanke"]
 
-    Twat::Twat.new.cli_run
+      Twat::Twat.new.cli_run
+    end
   end #}}}
 
   it "Should retrieve n tweets for user if invoked with count" do #{{{
-    tweet = mock()
-    Twitter.expects(:user_timeline).with("hanke", :count => 3).returns([tweet, tweet, tweet])
-    Twat::Subcommands::Finger.any_instance.expects(:format).with(tweet).times(3)
+    with_config(multiuser_config) do
+      tweet = mock()
+      Twitter.expects(:user_timeline).with("hanke", :count => 3).returns([tweet, tweet, tweet])
+      Twat::Subcommands::Finger.any_instance.expects(:format).with(tweet).times(3)
 
-    set_argv ["finger", "hanke", "3"]
+      set_argv ["finger", "hanke", "3"]
 
-    Twat::Twat.new.cli_run
+      Twat::Twat.new.cli_run
+    end
   end #}}}
 
 end
