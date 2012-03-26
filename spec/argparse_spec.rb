bdc69b5b89ef40460d63f55971d3ec1688784134
richo <richo@psych0tik.net>
2012-03-26 22:43:39 +1100

    Add both flags, make test data readable

--- spec/argparse_spec.rb
+++ spec/argparse_spec.rb
@@ -2,13 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 MULTIUSER_CONFIG = { accounts: {
         :rich0H =>
-          { oauth_token: "I'mtotallyatokenbrah",
-            oauth_token_secret: "I'mtotallyasecretbrah",
+          { oauth_token: "rich0h's token",
+            oauth_token_secret: "rich0H's secret",
             endpoint: :twitter
           },
         :secondAccount =>
-          { oauth_token: "I'mtotallyatokenbra__h",
-            oauth_token_secret: "I'mtotallyasecretbra__h",
+          { oauth_token: "secondAccount's token",
+            oauth_token_secret: "secondAccount's secret",
             endpoint: :twitter
           }
       },
@@ -21,6 +21,18 @@ describe Twat do
     with_config(MULTIUSER_CONFIG) do
 
       set_argv ["-n", "secondAccount"]
+
+      $args = ::Twat::Args.new
+      conf = ::Twat::Config.new
+
+      conf.account.should == MULTIUSER_CONFIG[:accounts][:secondAccount]
+    end
+  end
+
+  it "Should respect the --account flag" do
+    with_config(MULTIUSER_CONFIG) do
+
+      set_argv ["--account", "secondAccount"]
 
       $args = ::Twat::Args.new
       conf = ::Twat::Config.new
