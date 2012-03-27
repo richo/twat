14555f6cf7f7a9d114b4566782e3703c3b6e1eb9
richo <richo@psych0tik.net>
2012-03-27 21:08:04 +1100

    Start on a spec for config action

--- spec/subcommands/config_spec.rb
+++ spec/subcommands/config_spec.rb
@@ -2,7 +2,25 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Twat do
 
-  it "Should bail if add is called without an argument" do #{{{
-  end
+  multiuser_config = { accounts: {
+    :rich0H =>
+    { oauth_token: "rich0h's token",
+      oauth_token_secret: "rich0H's secret",
+      endpoint: :twitter
+    },
+      :secondAccount =>
+    { oauth_token: "secondAccount's token",
+      oauth_token_secret: "secondAccount's secret",
+      endpoint: :twitter
+    }
+  },
+    :default => :rich0H
+  }
+
+  # it "Should bail if config already exists" do #{{{
+  #   with_config(multiuser_config) do
+  #     set_argv ["config"]
+  #   end
+  # end
 
 end
