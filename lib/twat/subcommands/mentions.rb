7786e2fcf85925ff089bd6556299e87b19237175
richo <richo@psych0tik.net>
2012-03-29 20:06:24 +1100

    Mentions doesn't take arguments

--- lib/twat/subcommands/mentions.rb
+++ lib/twat/subcommands/mentions.rb
@@ -2,6 +2,11 @@ module Twat::Subcommands
   class Mentions < Base
   include FollowMixin
 
+  def run
+    needs_arguments(0)
+    super
+  end
+
   def self.usage
     "Usage: twat mentions"
   end
