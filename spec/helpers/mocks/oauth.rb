5c0e94f06ef1d282908f95870c5ee6e6e031d12b
Rich Healey <richard.healey@99designs.com>
2012-06-28 22:56:59 +1000

    Create mocks without any helpers

--- spec/helpers/mocks/oauth.rb
+++ spec/helpers/mocks/oauth.rb
@@ -1,14 +1,14 @@
 STUB_URL = "url"
 STUB_PIN = "1234"
 def mock_request_token
-  t = mock()
+  t = ::Mocha::Mock.new(Object)
   t.expects(:authorize_url).returns(STUB_URL)
   t.expects(:get_access_token).with(oauth_verifier: STUB_PIN).returns(mock_access_token)
   return t
 end
 
 def mock_access_token
-  t = mock()
+  t = ::Mocha::Mock.new(Object)
   t.expects(:token).returns("I'mtotallyatokenbrah")
   t.expects(:secret).returns("I'mtotallyasecretbrah")
   return t
